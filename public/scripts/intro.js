f10d32ba3a387d865c9f4a911ab3b26bdd732483
Vinicius <vinicius.sabino@faculdadesapiens.edu.br>
2020-06-10 19:13:07 -0400

    first commit

---
+++ public/scripts/intro.js
@@ -0,0 +1,70 @@
+// comments
+// document.write("Hello")
+
+// variaveis, tipos de dados
+// let myvar = "He"
+// const myconst = "He"
+
+// document.write(myconst + myvar)
+
+// string
+// "Isso é uma string"
+// 'Isso também é uma string'
+// `Isso é uma string também`
+
+// number
+// const n1 = 1
+// const n2 = 12
+// document.write(n1 + n2)
+
+// boolean - true ou false
+// const bTrue = True
+// const bFalse = false
+// document.write(bFalse)
+
+// objeto possuem propriedades e funcionalidade
+// const pessoa = {
+//   altura: "1,80m",
+//   idade: 24,
+//   solteiro: true,
+//   correr(){
+//     document.write("executar uma grande logida de correr")
+//   }
+// }
+
+// pessoa.correr()
+
+// Array - Vetores
+// const colecaoDeBolinhas = ["blue", "green", 1, {name:"My Name"}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+// Funções - Funcionalidades - Atalhos - Reuso de código
+
+// registrar função
+
+// function sayMyName(name) {
+  
+//   document.write(name)
+// }
+
+// // executar
+// sayMyName("Vini")
+// sayMyName("Clad")
+// sayMyName("Rod")
+
+// condicionais
+
+// const notaFinal = 7
+
+// if( notaFinal < 5 ) {
+//   document.write("Reprovado")  
+// } else {
+//   document.write("Aprovado")
+// }
+
+// loop repetições
+
+// for (i = 0; i < 10; i++) {
+//   document.write(`<p> ${i} </>`)
+// }
